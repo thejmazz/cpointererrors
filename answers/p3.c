bac360886b4460a8b537d66597a133360bb90b5a
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-03-04 00:08:03 -0500

    added answers for p3

---
+++ answers/p3.c
@@ -0,0 +1,28 @@
+// P - dereferencing a pointer w/o having allocated memory for it
+// D - deallocating memory that has already been deallocated
+// H - deallocating memory that is not located on the heap
+// M - memory leak
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int a=0, b=1, c=2;
+	int *p, *q, *r;
+
+	q = &c;
+	p = &a;
+	*q = b;
+	q = malloc(sizeof(int));
+	r = q;
+	*r = a + *q + c;
+	free(p); // H
+	r = malloc(sizeof(int));
+	free(q);
+	a = b + c + *r;
+	*q = *r; // P
+	free(r);
+
+	return 0;
+}
